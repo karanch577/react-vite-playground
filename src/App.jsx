c7db6b19ac7b6dd8b205219b8f48e25c94a56ec9
= <=>
2024-01-19 19:52:51 +0530

    Browser router updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 
 function App() {
 	return (
+		<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/product/:id" element={<Product />} />
 			<Route path="/cart" element={<Cart />} />
 		</Routes>
+		</BrowserRouter>
 	);
 }
 
